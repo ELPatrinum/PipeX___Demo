4e45cccea300bb54d8504c8f6da26d427ae369e4
elpatrinum <meb-bak@outlook.fr>
2023-12-10 17:01:39 +0100

    .

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: muel-bak <muel-bak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 15:31:38 by muel-bak          #+#    #+#             */
-/*   Updated: 2023/12/10 16:40:04 by muel-bak         ###   ########.fr       */
+/*   Updated: 2023/12/10 16:57:13 by muel-bak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,36 @@
 
 int pipex(char **av, char **env)
 {
-	int pid;
+	pid_t pid;
+	pid_t pid2;
 	int pipid[2];
 	int fd;
-	int dupfd;
+	int dupfd1;
+	int dupfd2;
 	if (pipe(pipid) == -1)
 		return (1);
 	pid = fork();
 	if (pid == 0)
 	{
 		if((fd = open(av[1], O_WRONLY, 0777)) == -1)
-			perror("open C");
-		dupfd = dup2(fd, STDOUT_FILENO);
+			perror("open c1");
+		dupfd1 = dup2(fd, STDOUT_FILENO);
+		dupfd2 = dup2(pipid[1], dupfd1);
 		if (execve(to_space(av[2]), ft_split(av[2],  ' '), env) == -1)
-			perror("execve C");
+			perror("execve c1");
 	}
-	else
+	pid2 = fork();
+	if (pid2 == 0)
 	{
-		// if ((fd = open(av[4], O_RDONLY, 0777)) == -1)
-		// 	perror("open P");
-		// dupfd  = dup2(fd, 1);
-		// if (execve(to_space(av[3]), ft_split(av[3], ' '), env) == -1)
-		// 	perror("execve P");
+		if ((fd = open(av[4], O_RDONLY, 0777)) == -1)
+			perror("open c2");
+		dupfd1 = dup2(fd, STDIN_FILENO);
+		dupfd2 = dup2(pipid[0], dupfd1);
+		if (execve(to_space(av[3]), ft_split(av[3], ' '), env) == -1)
+			perror("execve c2");		
 	}
+	waitpid(pid, NULL, 0);
+	waitpid(pid2, NULL, 0);
    return 0;
 }
 
