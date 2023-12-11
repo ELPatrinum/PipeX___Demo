eb364603e061fc4cae1094b71c4c3cd61a0e0fd4
elpatrinum <meb-bak@outlook.fr>
2023-12-11 18:26:20 +0100

    yeah

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: muel-bak <muel-bak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 14:48:35 by muel-bak          #+#    #+#             */
-/*   Updated: 2023/12/11 14:53:26 by muel-bak         ###   ########.fr       */
+/*   Updated: 2023/12/11 15:03:34 by muel-bak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	sec_cmd(char **av, char **env, int *pipid, int fd)
 	dup2(pipid[0], STDIN_FILENO);
 	dup2(fd, STDOUT_FILENO);
 	close(pipid[0]);
-	if (execve("/bin/sh", (char *[]){"sh", "-c", av[3], NULL}, env) == -1)
+	if (execve("/bin/zsh", (char *[]){"zsh", "-c", av[3], NULL}, env) == -1)
 	{
 		perror("execve");
 		exit(2);
